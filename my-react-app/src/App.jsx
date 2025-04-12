6bdda885234d9c2b205f9fb1d41bdddcb8a5893a
jared <jaredrotich3@gmail.com>
2025-04-12 23:38:15 +0300

    worked on add and search.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -22,13 +22,24 @@ function App() {
     }
   };
 
-  
+
   return (
-    <>
-      
-        
-      
-    </>
+    
+    <form onSubmit={handleAddExpense}>
+    <input
+      type="text"
+      placeholder="Expense Description"
+      value={description}
+      onChange={(e) => setDescription(e.target.value)}
+    />
+    <input
+      type="number"
+      placeholder="Amount"
+      value={amount}
+      onChange={(e) => setAmount(e.target.value)}
+    />
+    <button type="submit">Add Expense</button>
+  </form>
   )
 }
 
--- my-react-app/src/Search.jsx
+++ my-react-app/src/Search.jsx
@@ -5,4 +5,19 @@ const filteredExpenses = expenses.filter((expense) =>
     expense.description.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  return (
+    <div className="App">
+      <h1>Expense Tracker</h1>
+
+      
+      <input
+        type="text"
+        placeholder="Search by description"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      </div>
+
+  )
+
   export default Search;
