8966f689ffd6907c0258055bf26de4d2385033fd
jared <jaredrotich3@gmail.com>
2025-04-12 19:01:55 +0300

    added state to store expenses

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  //state to store expenses
   const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([]);
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [searchTerm, setSearchTerm] = useState("")
 
   return (
     <>
