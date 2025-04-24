3879b8a8bebfa1a3e4ca06a764fd8a8b4a38005b
jared <jaredrotich3@gmail.com>
2025-04-24 23:48:17 +0300

    added expenseform

---
+++ my-react-app/src/ExpenseForm.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+ 
+ function ExpenseForm({ setExpenses }) {
+   const [formData, setFormData] = useState({
+     name: "",
+     description: "",
+     category: "",
+     amount: "",
+     date: "",
+   });
+ 
+   function handleChange(e) {
+     setFormData({ ...formData, [e.target.name]: e.target.value });
+   }
+ 
+   function handleSubmit(e) {
+     e.preventDefault();
+     setExpenses((prev) => [...prev, formData]);
+     setFormData({
+       name: "",
+       description: "",
+       category: "",
+       amount: "",
+       date: "",
+     });
+   }
+ 
+   return (
+     <form onSubmit={handleSubmit}>
+       <h3>Add Expense</h3>
+       <input
+         type="text"
+         name="name"
+         placeholder="Expense name"
+         value={formData.name}
+         onChange={handleChange}
+         required
+       />
+       <input
+         type="text"
+         name="description"
+         placeholder="Enter expense description"
+         value={formData.description}
+         onChange={handleChange}
+       />
+       <input
+         type="text"
+         name="category"
+         placeholder="Enter category"
+         value={formData.category}
+         onChange={handleChange}
+       />
+       <input
+         type="number"
+         name="amount"
+         placeholder="Enter amount"
+         value={formData.amount}
+         onChange={handleChange}
+       />
+       <input
+         type="date"
+         name="date"
+         value={formData.date}
+         onChange={handleChange}
+       />
+       <button type="submit">Submit</button>
+     </form>
+   );
+ }
+ 
+ export default ExpenseForm;
